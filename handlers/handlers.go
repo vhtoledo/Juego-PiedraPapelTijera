65677da6609a6f9588f5ad18bf20f8d39993fb81
vhtoledo <querynetworking@gmail.com>
2023-08-14 11:06:04 -0300

    optimizacion con must

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,22 +2,18 @@ package handlers
 
 import (
 	"fmt"
-	"net/http"
 	"html/template"
+	"log"
+	"net/http"
+)
+
+const (
+	templateDir = "templates/"
+	templateBase = templateDir + "base.html"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	tpl, err := template.ParseFiles("templates/base.html", "templates/index.html")
-	if err != nil {
-		http.Error(w, "Error al analizar plantillas", http.StatusInternalServerError)
-		return
-	}
-
-	err = tpl.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
-		return
-	}
+	renderTemplate(w, "index.html", nil)
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
@@ -34,4 +30,15 @@ func Play(w http.ResponseWriter, r *http.Request) {
 
 func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Acerca de")
+}
+
+func renderTemplate(w http.ResponseWriter, page string, data any){
+	tpl := template.Must(template.ParseFiles(templateBase, templateDir+page))
+	
+	err := tpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
 }
